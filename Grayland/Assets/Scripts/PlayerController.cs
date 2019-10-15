ed67408939ccd0c6642257a671dcf18295d365f7
Demmy Mangusso <DemmyPlays@gmail.com>
2019-10-15 11:10:08 -0400

    Player controller start and folders

---
+++ Grayland/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Ground Movement")]
+    [SerializeField] private float groundSpeed;
+    [SerializeField] private float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    [Header("Jump Movement")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.velocity = jumpForce * Vector2.up;
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(groundSpeed * moveInput, rb.velocity.y);
+    }
+}
