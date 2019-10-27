3f2c38be764386796c2a5a09205faeaae12c14d1
Demmy Mangusso <DemmyPlays@gmail.com>
2019-10-27 19:40:08 -0400

    Particles implemented

--- Grayland/Assets/Scripts/PlayerController.cs
+++ Grayland/Assets/Scripts/PlayerController.cs
@@ -33,17 +33,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Tooltip("Child object that holds eyes sprite")]
     Transform eyes;
 
+    [SerializeField, Tooltip("Particle object for jumping, landing, and clinging to walls")]
+    GameObject particleObj;
+
     BoxCollider2D col;
     Vector2 velocity;
-    bool grounded;
+    bool grounded, landed;
     bool isClinged = false, canMove = true;
     float currentGravity;
 
     #endregion
-
-    // Do wall clinging by changing gravity when the player collides with a wall.
-    // Ground/Airborne = 0f, -gravity
-    // Wall/Ceiling = 0f, 0f
 
     private void Awake()
     {
@@ -61,8 +60,12 @@ public class PlayerController : MonoBehaviour
             velocity.y = 0;
 
             if (Input.GetButtonDown("Jump"))
+            {
                 // Velocity needed to achieve jump height
                 velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(currentGravity));
+
+                landed = false;
+            }
         }
         
         // Determine accel and decel values based on if the player is grounded or not
@@ -86,6 +89,17 @@ public class PlayerController : MonoBehaviour
         // Get overlapping colliders after velocity is applied
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, col.size, 0);
 
+        // Wall cling checking 
+        RaycastHit2D leftCheck = Physics2D.Raycast(transform.position, Vector2.left, .51f, whatIsCling); // Left side
+        RaycastHit2D rightCheck = Physics2D.Raycast(transform.position, Vector2.right, .51f, whatIsCling); // Right side
+        Debug.DrawRay(transform.position, new Vector2(-.51f, 0f), Color.green);
+        Debug.DrawRay(transform.position, new Vector2(.51f, 0f), Color.red);
+        isClinged = leftCheck || rightCheck; // Determine if the player is clinged to a wall
+
+        // Fix for ensuring landed is false when player is in the air
+        if (hits.Length == 1 && !isClinged)
+            landed = false;
+
         // Ground checking
         foreach (Collider2D hit in hits)
         {
@@ -102,25 +116,36 @@ public class PlayerController : MonoBehaviour
 
                 // If the object beneath us intersects, grounded is true 
                 if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                {
                     grounded = true;
+
+                    // Ensures particle only spawns once
+                    if (!landed)
+                    {
+                        Instantiate(particleObj, new Vector2(transform.position.x, transform.position.y - .5f), Quaternion.identity);
+                        landed = true;
+                    }
+                }
             }
         }
-
-        // Wall cling checking 
-        RaycastHit2D leftCheck = Physics2D.Raycast(transform.position, Vector2.left, .55f, whatIsCling); // Left side
-        RaycastHit2D rightCheck = Physics2D.Raycast(transform.position, Vector2.right, .55f, whatIsCling); // Right side
-        Debug.DrawRay(transform.position, new Vector2(-.55f, 0f), Color.green);
-        Debug.DrawRay(transform.position, new Vector2(.55f, 0f), Color.red);
-        isClinged = leftCheck || rightCheck; // Determine if the player is clinged to a wall
 
         // If player is clinged, stop and lock movement
         if (isClinged)
         {
-            Debug.Log("Clinged");
             currentGravity = 0f;
             velocity = Vector2.zero;
             moveInput = 0f;
             canMove = false;
+
+            // Ensures particle only spawns once
+            if (!landed)
+            {
+                if (leftCheck)
+                    Instantiate(particleObj, new Vector2(transform.position.x -.5f, transform.position.y), Quaternion.Euler(0f, 0f, -90f));
+                else
+                    Instantiate(particleObj, new Vector2(transform.position.x +.5f, transform.position.y), Quaternion.Euler(0f, 0f, 90f));
+                landed = true;
+            }
 
             if (Input.GetButtonDown("Jump"))
             {
@@ -129,11 +154,14 @@ public class PlayerController : MonoBehaviour
                 velocity = new Vector2(speedX, Mathf.Sqrt(2f * jumpHeight * Mathf.Abs(currentGravity)));
                 
                 isClinged = false;
+                landed = false;
                 StartCoroutine(SetCanMove(true));
             }
         }
 
         eyes.localPosition = new Vector2(Mathf.Lerp(eyes.localPosition.x, velocity.x / 80, .25f), Mathf.Lerp(eyes.localPosition.y, velocity.y / 100, .25f));
+
+        Debug.Log(landed);
     }
 
     /// <summary>
